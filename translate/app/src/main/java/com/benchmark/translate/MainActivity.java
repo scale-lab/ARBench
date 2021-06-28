13ad9aad4021f1a930739d45457ba064e6d1659e
Tetracyl <seifaziz10@gmail.com>
2021-06-28 16:47:03 -0700

    Add ocr and translation

--- translate/app/src/main/java/com/benchmark/translate/MainActivity.java
+++ translate/app/src/main/java/com/benchmark/translate/MainActivity.java
@@ -1,14 +1,126 @@
 package com.benchmark.translate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.benchmark.ocr.*;
+
+import static com.benchmark.ocr.OcrCaptureActivity.TextBlockObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private final int CAMERA_SCAN_TEXT = 0;
+    private final int LOAD_IMAGE_RESULTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionCamera:
+                com.benchmark.ocr.OCRCapture.Builder(this)
+                        .setUseFlash(false)
+                        .setAutoFocus(true)
+                        .buildWithRequestCode(CAMERA_SCAN_TEXT);
+                break;
+            case R.id.actionPhoto:
+                if (hasPermission()) {
+                    pickImage();
+                } else {
+                    getPermission();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getPermission() {
+// Permission is not granted
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //TODO:
+        } else {
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    pickImage();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void pickImage() {
+        Intent intentGallery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intentGallery, LOAD_IMAGE_RESULTS);
+    }
+
+    private boolean hasPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (requestCode == CAMERA_SCAN_TEXT) {
+                if (resultCode == CommonStatusCodes.SUCCESS) {
+                    textView.setText(data.getStringExtra(TextBlockObject));
+                }
+            } else if (requestCode == LOAD_IMAGE_RESULTS) {
+                Uri pickedImage = data.getData();
+                String text = OCRCapture.Builder(this).getTextFromUri(pickedImage);
+                textView.setText(text);
+            }
+        }
     }
 }
