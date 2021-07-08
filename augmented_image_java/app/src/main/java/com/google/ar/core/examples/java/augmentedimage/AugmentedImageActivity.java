6cb9365f740541ca0321f7ec85a5006f4a27217d
Tetracyl <seifaziz10@gmail.com>
2021-07-08 11:18:08 -0700

    Add recording and playback to Augmented Image app

--- augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/AugmentedImageActivity.java
+++ augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/AugmentedImageActivity.java
@@ -16,18 +16,34 @@
 
 package com.google.ar.core.examples.java.augmentedimage;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.loader.content.CursorLoader;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.google.ar.core.Anchor;
@@ -37,6 +53,9 @@ import com.google.ar.core.AugmentedImageDatabase;
 import com.google.ar.core.Camera;
 import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
+import com.google.ar.core.PlaybackStatus;
+import com.google.ar.core.RecordingConfig;
+import com.google.ar.core.RecordingStatus;
 import com.google.ar.core.Session;
 import com.google.ar.core.examples.java.augmentedimage.rendering.AugmentedImageRenderer;
 import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
@@ -46,13 +65,20 @@ import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
 import com.google.ar.core.examples.java.common.helpers.TrackingStateHelper;
 import com.google.ar.core.examples.java.common.rendering.BackgroundRenderer;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.PlaybackFailedException;
+import com.google.ar.core.exceptions.RecordingFailedException;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.microedition.khronos.egl.EGLConfig;
@@ -96,6 +122,21 @@ public class AugmentedImageActivity extends AppCompatActivity implements GLSurfa
   // database.
   private final Map<Integer, Pair<AugmentedImage, Anchor>> augmentedImageMap = new HashMap<>();
 
+  // Represents the app's working state.
+  public enum AppState {
+    Idle,
+    Recording,
+    Playingback
+  }
+
+  // Tracks app's specific state changes.
+  private AppState appState = AppState.Idle;
+
+  private final String MP4_VIDEO_MIME_TYPE = "video/mp4";
+  private final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+  private int REQUEST_MP4_SELECTOR = 1;
+  private boolean hasSetTextureNames = false;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -118,6 +159,433 @@ public class AugmentedImageActivity extends AppCompatActivity implements GLSurfa
         .into(fitToScanView);
 
     installRequested = false;
+  }
+
+  private void updateRecordButton() {
+    View buttonView = findViewById(R.id.record_button);
+    Button button = (Button)buttonView;
+
+    switch (appState) {
+
+      // The app is neither recording nor playing back. The "Record" button is visible.
+      case Idle:
+        button.setText("Record");
+        button.setVisibility(View.VISIBLE);
+        break;
+
+      // While recording, the "Record" button is visible and says "Stop".
+      case Recording:
+        button.setText("Stop");
+        button.setVisibility(View.VISIBLE);
+        break;
+
+      // During playback, the "Record" button is not visible.
+      case Playingback:
+        button.setVisibility(View.INVISIBLE);
+        break;
+    }
+  }
+
+  public void onClickRecord(View view) {
+    Log.d(TAG, "onClickRecord");
+
+    // Check the app's internal state and switch to the new state if needed.
+    switch (appState) {
+      // If the app is not recording, begin recording.
+      case Idle: {
+        boolean hasStarted = startRecording();
+        Log.d(TAG, String.format("onClickRecord start: hasStarted %b", hasStarted));
+
+        if (hasStarted)
+          appState = AppState.Recording;
+
+        break;
+      }
+
+      // If the app is recording, stop recording.
+      case Recording: {
+        boolean hasStopped = stopRecording();
+        Log.d(TAG, String.format("onClickRecord stop: hasStopped %b", hasStopped));
+
+        if (hasStopped)
+          appState = AppState.Idle;
+
+        break;
+      }
+
+      default:
+        // Do nothing.
+        break;
+    }
+
+    updateRecordButton();
+    updatePlaybackButton();
+  }
+
+  public void onClickPlayback(View view) {
+    Log.d(TAG, "onClickPlayback");
+
+    switch (appState) {
+
+      // If the app is not playing back, open the file picker.
+      case Idle: {
+        boolean hasStarted = selectFileToPlayback();
+        Log.d(TAG, String.format("onClickPlayback start: selectFileToPlayback %b", hasStarted));
+        break;
+      }
+
+      // If the app is playing back, stop playing back.
+      case Playingback: {
+        boolean hasStopped = stopPlayingback();
+        Log.d(TAG, String.format("onClickPlayback stop: hasStopped %b", hasStopped));
+        break;
+      }
+
+      default:
+        // Recording - do nothing.
+        break;
+    }
+
+    // Update the UI for the "Record" and "Playback" buttons.
+    updateRecordButton();
+    updatePlaybackButton();
+  }
+
+  private boolean selectFileToPlayback() {
+    // Start file selection from Movies directory.
+    // Android 10 and above requires VOLUME_EXTERNAL_PRIMARY to write to MediaStore.
+    Uri videoCollection;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      videoCollection = MediaStore.Video.Media.getContentUri(
+              MediaStore.VOLUME_EXTERNAL_PRIMARY);
+    } else {
+      videoCollection = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+    }
+
+    // Create an Intent to select a file.
+    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+    // Add file filters such as the MIME type, the default directory and the file category.
+    intent.setType(MP4_VIDEO_MIME_TYPE); // Only select *.mp4 files
+    intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, videoCollection); // Set default directory
+    intent.addCategory(Intent.CATEGORY_OPENABLE); // Must be files that can be opened
+
+    this.startActivityForResult(intent, REQUEST_MP4_SELECTOR);
+
+    return true;
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    // Check request status. Log an error if the selection fails.
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode != Activity.RESULT_OK || requestCode != REQUEST_MP4_SELECTOR) {
+      Log.e(TAG, "onActivityResult select file failed");
+      return;
+    }
+
+    Uri mp4Uri = data.getData();
+    Log.d(TAG, String.format("onActivityResult result is %s", mp4Uri));
+
+    // Copy to app internal storage to get a file path.
+    String localFilePath = copyToInternalFilePath(mp4Uri);
+
+    // Begin playback.
+    startPlayingback(localFilePath);
+  }
+
+  private String copyToInternalFilePath(Uri contentUri) {
+    // Create a file path in the app's internal storage.
+    String tempPlaybackFilePath = new File(this.getExternalFilesDir(null), "temp-playback.mp4").getAbsolutePath();
+
+    // Copy the binary content from contentUri to tempPlaybackFilePath.
+    try (InputStream inputStream = this.getContentResolver().openInputStream(contentUri);
+         java.io.OutputStream tempOutputFileStream = new java.io.FileOutputStream(tempPlaybackFilePath)) {
+
+      byte[] buffer = new byte[1024 * 1024]; // 1MB
+      int bytesRead = inputStream.read(buffer);
+      while (bytesRead != -1) {
+        tempOutputFileStream.write(buffer, 0, bytesRead);
+        bytesRead = inputStream.read(buffer);
+      }
+
+    } catch (java.io.FileNotFoundException e) {
+      Log.e(TAG, "copyToInternalFilePath FileNotFoundException", e);
+      return null;
+    } catch (IOException e) {
+      Log.e(TAG, "copyToInternalFilePath IOException", e);
+      return null;
+    }
+
+    // Return the absolute file path of the copied file.
+    return tempPlaybackFilePath;
+  }
+
+  private boolean startPlayingback(String mp4FilePath) {
+    if (mp4FilePath == null)
+      return false;
+
+    Log.d(TAG, "startPlayingback at:" + mp4FilePath);
+
+    pauseARCoreSession();
+
+    try {
+      session.setPlaybackDataset(mp4FilePath);
+    } catch (PlaybackFailedException e) {
+      Log.e(TAG, "startPlayingback - setPlaybackDataset failed", e);
+    }
+
+    // The session's camera texture name becomes invalid when the
+    // ARCore session is set to play back.
+    // Workaround: Reset the Texture to start Playback
+    // so it doesn't crashes with AR_ERROR_TEXTURE_NOT_SET.
+    hasSetTextureNames = false;
+
+    boolean canResume = resumeARCoreSession();
+    if (!canResume)
+      return false;
+
+    PlaybackStatus playbackStatus = session.getPlaybackStatus();
+    Log.d(TAG, String.format("startPlayingback - playbackStatus %s", playbackStatus));
+
+
+    if (playbackStatus != PlaybackStatus.OK) { // Correctness check
+      return false;
+    }
+
+    appState = AppState.Playingback;
+    updateRecordButton();
+    updatePlaybackButton();
+
+    return true;
+  }
+
+  private boolean stopPlayingback() {
+    // Correctness check, only stop playing back when the app is playing back.
+    if (appState != AppState.Playingback)
+      return false;
+
+    pauseARCoreSession();
+
+    // Close the current session and create a new session.
+    session.close();
+    try {
+      session = new Session(this);
+    } catch (UnavailableArcoreNotInstalledException
+            |UnavailableApkTooOldException
+            |UnavailableSdkTooOldException
+            | UnavailableDeviceNotCompatibleException e) {
+      Log.e(TAG, "Error in return to Idle state. Cannot create new ARCore session", e);
+      return false;
+    }
+    configureSession();
+
+    boolean canResume = resumeARCoreSession();
+    if (!canResume)
+      return false;
+
+    // A new session will not have a camera texture name.
+    // Manually set hasSetTextureNames to false to trigger a reset.
+    hasSetTextureNames = false;
+
+    // Reset appState to Idle, and update the "Record" and "Playback" buttons.
+    appState = AppState.Idle;
+    updateRecordButton();
+    updatePlaybackButton();
+
+    return true;
+  }
+
+  private void updatePlaybackButton() {
+    View buttonView = findViewById(R.id.playback_button);
+    Button button = (Button)buttonView;
+
+    switch (appState) {
+
+      // The app is neither recording nor playing back. The "Playback" button is visible.
+      case Idle:
+        button.setText("Playback");
+        button.setVisibility(View.VISIBLE);
+        break;
+
+      // While playing back, the "Playback" button is visible and says "Stop".
+      case Playingback:
+        button.setText("Stop");
+        button.setVisibility(View.VISIBLE);
+        break;
+
+      // During recording, the "Playback" button is not visible.
+      case Recording:
+        button.setVisibility(View.INVISIBLE);
+        break;
+    }
+  }
+
+  private boolean startRecording() {
+    String mp4FilePath = createMp4File();
+    if (mp4FilePath == null)
+      return false;
+
+    Log.d(TAG, "startRecording at: " + mp4FilePath);
+
+    pauseARCoreSession();
+
+    // Configure the ARCore session to start recording.
+    RecordingConfig recordingConfig = new RecordingConfig(session)
+            .setMp4DatasetFilePath(mp4FilePath)
+            .setAutoStopOnPause(true);
+
+    try {
+      // Prepare the session for recording, but do not start recording yet.
+      session.startRecording(recordingConfig);
+    } catch (RecordingFailedException e) {
+      Log.e(TAG, "startRecording - Failed to prepare to start recording", e);
+      return false;
+    }
+
+    boolean canResume = resumeARCoreSession();
+    if (!canResume)
+      return false;
+
+    // Correctness checking: check the ARCore session's RecordingState.
+    RecordingStatus recordingStatus = session.getRecordingStatus();
+    Log.d(TAG, String.format("startRecording - recordingStatus %s", recordingStatus));
+    return recordingStatus == RecordingStatus.OK;
+  }
+
+  private void pauseARCoreSession() {
+    // Pause the GLSurfaceView so that it doesn't update the ARCore session.
+    // Pause the ARCore session so that we can update its configuration.
+    // If the GLSurfaceView is not paused,
+    //   onDrawFrame() will try to update the ARCore session
+    //   while it's paused, resulting in a crash.
+    surfaceView.onPause();
+    session.pause();
+  }
+
+  private boolean resumeARCoreSession() {
+    // We must resume the ARCore session before the GLSurfaceView.
+    // Otherwise, the GLSurfaceView will try to update the ARCore session.
+    try {
+      session.resume();
+    } catch (CameraNotAvailableException e) {
+      Log.e(TAG, "CameraNotAvailableException in resumeARCoreSession", e);
+      return false;
+    }
+
+    surfaceView.onResume();
+    return true;
+  }
+
+  private boolean stopRecording() {
+    try {
+      session.stopRecording();
+    } catch (RecordingFailedException e) {
+      Log.e(TAG, "stopRecording - Failed to stop recording", e);
+      return false;
+    }
+
+    // Correctness checking: check if the session stopped recording.
+    return session.getRecordingStatus() == RecordingStatus.NONE;
+  }
+
+  private String createMp4File() {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+      if (!checkAndRequestStoragePermission()) {
+        Log.i(TAG, String.format(
+                "Didn't createMp4File. No storage permission, API Level = %d",
+                Build.VERSION.SDK_INT));
+        return null;
+      }
+    }
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+    String mp4FileName = "arcore-" + dateFormat.format(new Date()) + ".mp4";
+
+    ContentResolver resolver = this.getContentResolver();
+
+    Uri videoCollection = null;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      videoCollection = MediaStore.Video.Media.getContentUri(
+              MediaStore.VOLUME_EXTERNAL_PRIMARY);
+    } else {
+      videoCollection = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+    }
+
+    // Create a new Media file record.
+    ContentValues newMp4FileDetails = new ContentValues();
+    newMp4FileDetails.put(MediaStore.Video.Media.DISPLAY_NAME, mp4FileName);
+    newMp4FileDetails.put(MediaStore.Video.Media.MIME_TYPE, MP4_VIDEO_MIME_TYPE);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      // The Relative_Path column is only available since API Level 29.
+      newMp4FileDetails.put(MediaStore.Video.Media.RELATIVE_PATH, Environment.DIRECTORY_MOVIES);
+    } else {
+      // Use the Data column to set path for API Level <= 28.
+      File mp4FileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+      String absoluteMp4FilePath = new File(mp4FileDir, mp4FileName).getAbsolutePath();
+      newMp4FileDetails.put(MediaStore.Video.Media.DATA, absoluteMp4FilePath);
+    }
+
+    Uri newMp4FileUri = resolver.insert(videoCollection, newMp4FileDetails);
+
+    // Ensure that this file exists and can be written.
+    if (newMp4FileUri == null) {
+      Log.e(TAG, String.format("Failed to insert Video entity in MediaStore. API Level = %d", Build.VERSION.SDK_INT));
+      return null;
+    }
+
+    // This call ensures the file exist before we pass it to the ARCore API.
+    if (!testFileWriteAccess(newMp4FileUri)) {
+      return null;
+    }
+
+    String filePath = getMediaFilePath(newMp4FileUri);
+    Log.d(TAG, String.format("createMp4File = %s, API Level = %d", filePath, Build.VERSION.SDK_INT));
+
+    return filePath;
+  }
+
+  // Test if the file represented by the content Uri can be open with write access.
+  private boolean testFileWriteAccess(Uri contentUri) {
+    try (java.io.OutputStream mp4File = this.getContentResolver().openOutputStream(contentUri)) {
+      Log.d(TAG, String.format("Success in testFileWriteAccess %s", contentUri.toString()));
+      return true;
+    } catch (java.io.FileNotFoundException e) {
+      Log.e(TAG, String.format("FileNotFoundException in testFileWriteAccess %s", contentUri.toString()), e);
+    } catch (java.io.IOException e) {
+      Log.e(TAG, String.format("IOException in testFileWriteAccess %s", contentUri.toString()), e);
+    }
+
+    return false;
+  }
+
+  // Query the Media.DATA column to get file path from MediaStore content:// Uri
+  private String getMediaFilePath(Uri mediaStoreUri) {
+    String[] projection = { MediaStore.Images.Media.DATA };
+
+    CursorLoader loader = new CursorLoader(this, mediaStoreUri, projection, null, null, null);
+    Cursor cursor = loader.loadInBackground();
+    cursor.moveToFirst();
+
+    int data_column_index = cursor.getColumnIndexOrThrow(projection[0]);
+    String data_result = cursor.getString(data_column_index);
+
+    cursor.close();
+
+    return data_result;
+  }
+
+  public boolean checkAndRequestStoragePermission() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this,
+              new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+              REQUEST_WRITE_EXTERNAL_STORAGE);
+      return false;
+    }
+
+    return true;
   }
 
   @Override
@@ -263,17 +731,28 @@ public class AugmentedImageActivity extends AppCompatActivity implements GLSurfa
     if (session == null) {
       return;
     }
+
+    if (!hasSetTextureNames) {
+      session.setCameraTextureName(backgroundRenderer.getTextureId());
+      hasSetTextureNames = true;
+    }
+
     // Notify ARCore session that the view size changed so that the perspective matrix and
     // the video background can be properly adjusted.
     displayRotationHelper.updateSessionIfNeeded(session);
 
+    Frame frame;
     try {
-      session.setCameraTextureName(backgroundRenderer.getTextureId());
+      if (appState == AppState.Playingback
+              && session.getPlaybackStatus() == PlaybackStatus.FINISHED) {
+        this.runOnUiThread(this::stopPlayingback);
+        return;
+      }
 
       // Obtain the current frame from ARSession. When the configuration is set to
       // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
       // camera framerate.
-      Frame frame = session.update();
+      frame = session.update();
       Camera camera = frame.getCamera();
 
       // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
