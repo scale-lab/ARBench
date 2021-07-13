8393301b86d3b6d2bb46c57e7d75a1b010d6ae62
Tetracyl <seifaziz10@gmail.com>
2021-07-13 12:50:49 -0700

    Make camera run in background

---
+++ benchmark/app/src/main/java/benchmark/benchmark/CameraPreview.java
@@ -0,0 +1,45 @@
+package benchmark.benchmark;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.view.Gravity;
+import android.view.TextureView;
+import android.widget.FrameLayout;
+
+import java.io.IOException;
+
+public class CameraPreview extends TextureView implements TextureView.SurfaceTextureListener {
+
+    private Camera camera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        setLayoutParams(new FrameLayout.LayoutParams(
+                previewSize.width, previewSize.height, Gravity.CENTER));
+
+        try{
+            camera.setPreviewTexture(surface);
+        } catch (IOException t) {}
+
+        camera.startPreview();
+        this.setVisibility(INVISIBLE);
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) { }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) { }
+}
