f0c8f4e766da916a24be0b58e262a65a405f6930
Tetracyl <seifaziz10@gmail.com>
2021-07-19 15:53:39 -0700

    Replace common folder

--- ar-apps/augmented_object_recognition_2/app/src/main/java/com/google/ar/core/examples/java/common/helpers/TapHelper.java
+++ ar-apps/augmented_object_recognition_2/app/src/main/java/com/google/ar/core/examples/java/common/helpers/TapHelper.java
@@ -20,7 +20,6 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
