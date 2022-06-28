b2a99dea8df1d70d2c2c331db8d45ceab0315021
Seif <seifaziz10@gmail.com>
2022-06-28 13:16:14 -0700

    Update ML app package versions

--- ar-apps/augmented_object_recognition/app/src/main/java/com/google/ar/core/examples/java/ml/classification/MLKitObjectDetector.kt
+++ ar-apps/augmented_object_recognition/app/src/main/java/com/google/ar/core/examples/java/ml/classification/MLKitObjectDetector.kt
@@ -33,38 +33,42 @@ import kotlinx.coroutines.tasks.asDeferred
  * Analyzes an image using ML Kit.
  */
 class MLKitObjectDetector(context: Activity) : ObjectDetector(context) {
-  // To use a custom model, follow steps on https://developers.google.com/ml-kit/vision/object-detection/custom-models/android.
-  val model = LocalModel.Builder().setAssetFilePath("mnasnet_0.50_224_1_metadata_1.tflite").build()
-   val builder = CustomObjectDetectorOptions.Builder(model)
+    // To use a custom model, follow steps on https://developers.google.com/ml-kit/vision/object-detection/custom-models/android.
+    val model =
+        LocalModel.Builder().setAssetFilePath("mnasnet_0.50_224_1_metadata_1.tflite").build()
+    val builder = CustomObjectDetectorOptions.Builder(model)
 
-  // For the ML Kit default model, use the following:
+    // For the ML Kit default model, use the following:
 //  val builder = ObjectDetectorOptions.Builder()
 
-  private val options = builder
-    .setDetectorMode(CustomObjectDetectorOptions.SINGLE_IMAGE_MODE)
-    .enableClassification()
-    .enableMultipleObjects()
-    .build()
-  private val detector = ObjectDetection.getClient(options)
+    private val options = builder
+        .setDetectorMode(CustomObjectDetectorOptions.SINGLE_IMAGE_MODE)
+        .enableClassification()
+        .enableMultipleObjects()
+        .build()
+    private val detector = ObjectDetection.getClient(options)
 
-  override suspend fun analyze(image: Image, imageRotation: Int): List<DetectedObjectResult> {
-    // `image` is in YUV (https://developers.google.com/ar/reference/java/com/google/ar/core/Frame#acquireCameraImage()),
-    val convertYuv = convertYuv(image)
+    override suspend fun analyze(image: Image, imageRotation: Int): List<DetectedObjectResult> {
+        // `image` is in YUV (https://developers.google.com/ar/reference/java/com/google/ar/core/Frame#acquireCameraImage()),
+        val convertYuv = convertYuv(image)
 
-    // The model performs best on upright images, so rotate it.
-    val rotatedImage = ImageUtils.rotateBitmap(convertYuv, imageRotation)
+        // The model performs best on upright images, so rotate it.
+        val rotatedImage = ImageUtils.rotateBitmap(convertYuv, imageRotation)
 
-    val inputImage = InputImage.fromBitmap(rotatedImage, 0)
+        val inputImage = InputImage.fromBitmap(rotatedImage, 0)
 
-    val mlKitDetectedObjects = detector.process(inputImage).asDeferred().await()
-    return mlKitDetectedObjects.mapNotNull { obj ->
-      val bestLabel = obj.labels.maxByOrNull { label -> label.confidence } ?: return@mapNotNull null
-      val coords = obj.boundingBox.exactCenterX().toInt() to obj.boundingBox.exactCenterY().toInt()
-      val rotatedCoordinates = coords.rotateCoordinates(rotatedImage.width, rotatedImage.height, imageRotation)
-      DetectedObjectResult(bestLabel.confidence, bestLabel.text, rotatedCoordinates)
+        val mlKitDetectedObjects = detector.process(inputImage).asDeferred().await()
+        return mlKitDetectedObjects.mapNotNull { obj ->
+            val bestLabel =
+                obj.labels.maxByOrNull { label -> label.confidence } ?: return@mapNotNull null
+            val coords =
+                obj.boundingBox.exactCenterX().toInt() to obj.boundingBox.exactCenterY().toInt()
+            val rotatedCoordinates =
+                coords.rotateCoordinates(rotatedImage.width, rotatedImage.height, imageRotation)
+            DetectedObjectResult(bestLabel.confidence, bestLabel.text, rotatedCoordinates)
+        }
     }
-  }
 
-  @Suppress("USELESS_IS_CHECK")
-  fun hasCustomModel() = builder is CustomObjectDetectorOptions.Builder
+    @Suppress("USELESS_IS_CHECK")
+    fun hasCustomModel() = builder is CustomObjectDetectorOptions.Builder
 }
